a6882b7e8a46a3c804c46715e6fdbb0058aed29c
linux <linux@debian.fritz.box>
2013-07-25 21:32:28 +0200

    moved tests to tests/ and renamed appropriately

--- imagefilters/tests/rwtest.cpp
+++ imagefilters/tests/rwtest.cpp
@@ -1,4 +1,6 @@
-/*  Copyright (C) 2013  H. F. Noelscher
+/*  Test for image reading and writing
+
+    Copyright (C) 2013  H. F. Noelscher
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
